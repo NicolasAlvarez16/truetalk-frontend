9b08f5d9d92e71a6de3d926fe91ff9eb6fbe2234
Nicolas <nicoalavarezgarrido@gmail.com>
2022-12-04 19:00:45 +0000

    FYP-54 rightbar completed

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -2,7 +2,87 @@ import "./rightBar.scss"
 
 function RightBar() {
   return (
-    <div className="rightBar">RightBar</div>
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.kym-cdn.com/entries/icons/facebook/000/030/710/dd0.jpg" alt="" />
+              <span>MonkeyMeme</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.kym-cdn.com/entries/icons/facebook/000/030/710/dd0.jpg" alt="" />
+              <span>MonkeyMeme</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.kym-cdn.com/entries/icons/facebook/000/030/710/dd0.jpg" alt="" />
+              <p>
+                <span>MonkeyMeme</span> changed thier cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.kym-cdn.com/entries/icons/facebook/000/030/710/dd0.jpg" alt="" />
+              <p>
+                <span>MonkeyMeme</span> liked a post
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.kym-cdn.com/entries/icons/facebook/000/030/710/dd0.jpg" alt="" />
+              <p>
+                <span>MonkeyMeme</span> shared a post
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.kym-cdn.com/entries/icons/facebook/000/030/710/dd0.jpg" alt="" />
+              <div className="online"/>
+              <span>MonkeyMeme</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.kym-cdn.com/entries/icons/facebook/000/030/710/dd0.jpg" alt="" />
+              <div className="online"/>
+              <span>MonkeyMeme</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.kym-cdn.com/entries/icons/facebook/000/030/710/dd0.jpg" alt="" />
+              <div className="online"/>
+              <span>MonkeyMeme</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
